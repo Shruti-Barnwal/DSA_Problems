e4a6fb338d9d5cfb312f497a7bdf6388005dae7b
Shruti-Barnwal <shruti.bharti411@gmail.com>
2026-09-23 21:13:07 +0530

    Time: 0 ms (100%), Space: 10.2 MB (19.22%) - LeetHub

--- 3803-count-residue-prefixes/3803-count-residue-prefixes.cpp
+++ 3803-count-residue-prefixes/3803-count-residue-prefixes.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int residuePrefixes(string s) {
-        unordered_set<char>st;
-        int cnt = 0, i=0;
-        while(i<s.size()){
-            if(!st.count(s[i])) st.insert(s[i]);
-
-            if(st.size() == (i+1)%3) cnt++;
-    
-            i++;
+        int n=s.size();
+        unordered_map<char,int>m;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            m[s[i]]++;
+            if(m.size() == (i+1)%3) cnt++;
         }
-        
         return cnt;
     }
 };
