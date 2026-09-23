60e812de41888d7998d778ed8f52c792d7ac58ab
Shruti-Barnwal <shruti.bharti411@gmail.com>
2026-09-23 22:38:19 +0530

    Time: 0 ms (100%), Space: 9.4 MB (62.44%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool areOccurrencesEqual(string s) {
-        int n = s.size();
         int freq[26] = {0};
-
-        for(int i=0;i<n;i++){
+        for(int i=0;i<s.size();i++){
             freq[s[i] - 'a']++;
         }
 
-        int cnt = 0;
+        int ans = 0;
         for(int i=0;i<26;i++){
             if(freq[i] > 0){
-                cnt = freq[i];
+                ans = freq[i];
                 break;
             }
         }
 
         for(int i=0;i<26;i++){
-            if(freq[i] > 0){
-                if(freq[i] != cnt) return false;
+            if(freq[i] > 0 && freq[i] != ans){
+                return false;
             }
         }
         return true;
