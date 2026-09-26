970da1d5aec2ea0f4ed864c28b4ce9811cd3f8d6
Shruti-Barnwal <shruti.bharti411@gmail.com>
2026-09-26 19:40:16 +0530

    Time: 434 ms (20.5%), Space: 318.1 MB (16.77%) - LeetHub

--- 3795-minimum-subarray-length-with-distinct-sum-at-least-k/3795-minimum-subarray-length-with-distinct-sum-at-least-k.cpp
+++ 3795-minimum-subarray-length-with-distinct-sum-at-least-k/3795-minimum-subarray-length-with-distinct-sum-at-least-k.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int minLength(vector<int>& nums, int k) {
-        int n=nums.size();
-        unordered_map<int,int>mpp;
-        int l=0, sum = 0, minLen=INT_MAX;
+        int n = nums.size();
+        unordered_map<int,int>freq;
 
+        int l = 0, sum = 0, mini = INT_MAX;
         for(int r=0;r<n;r++){
-            mpp[nums[r]]++;
+            freq[nums[r]]++;
 
-            if(mpp[nums[r]] == 1){
-                sum += nums[r];  // sum contains only distinct element 
+            if(freq[nums[r]] == 1){
+                sum += nums[r];
             }
+
             while(sum >= k){
-                minLen = min(minLen, r-l+1);
-                mpp[nums[l]]--;
-                if(mpp[nums[l]] == 0){
-                    mpp.erase(nums[l]);
+                mini = min(mini, r-l+1);
+                freq[nums[l]]--;
+                if(freq[nums[l]] == 0){
+                    freq.erase(nums[l]);
                     sum -= nums[l];
                 }
                 l++;
             }
         }
-        return minLen == INT_MAX? -1:minLen;
+        return mini == INT_MAX? -1:mini;
     }
 };
